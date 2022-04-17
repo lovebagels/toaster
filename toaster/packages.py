da20860a86ab79464a79b577c2d4b5b06206bd6e
wxllow <35239049+wxllow@users.noreply.github.com>
2022-04-16 22:15:05 -0400

    Uninstall scripts

--- toaster/packages.py
+++ toaster/packages.py
@@ -27,6 +27,24 @@ def remove_package(package):
     package_toml = toml.load(
         f'/opt/toaster/sources/{package_source}/{package}/{package}.toml')
 
+    # Run "pre_scripts"
+    if dependingonsys(package_toml['build']['uninstall'], 'pre_scripts', append_mode=True):
+        for cmd in dependingonsys(package_toml['build']['uninstall'], 'pre_scripts', append_mode=True):
+            subprocess.run(cmd)
+
+    # Run make commands
+    if dependingonsys(package_toml['build']['uninstall'], 'make', append_mode=True):
+        for cmd in dependingonsys(package_toml['build']['uninstall'], 'make', append_mode=True):
+            if dependingonsys(package_toml['build']['uninstall'], 'make_sudo'):
+                subprocess.run(['sudo', 'make'] + cmd)
+            else:
+                subprocess.run(['make'] + cmd)
+
+    # Run "post_scripts"
+    if dependingonsys(package_toml['build']['uninstall'], 'post_scripts', append_mode=True):
+        for cmd in dependingonsys(package_toml['build']['uninstall'], 'post_scripts', append_mode=True):
+            subprocess.run(cmd)
+
     if os.path.exists(f'/opt/toaster/packages/{package}'):
         shutil.rmtree(f'/opt/toaster/packages/{package}')
     else:
