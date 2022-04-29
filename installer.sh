4af9d931798c32e8216e5d0f7d524e4594c876bf
wxllow <35239049+wxllow@users.noreply.github.com>
2022-04-29 19:26:11 -0400

    Semi-fancy (not really) installer

--- installer.sh
+++ installer.sh
@@ -1,9 +1,17 @@
+echo "Welcome to the toaster installer! 🍞"
+
+echo "Making /opt/toaster directory, your password may be required..."
 sudo mkdir /opt/toaster
-cp defaults/bakery.json /opt/toaster/bakery.json
+
+echo "Copying default bakeries..."
+sudo cp defaults/bakery.json /opt/toaster/bakery.json
+
+echo "Giving you ownership of /opt/toaster... 🔨"
 cd /opt/toaster
 sudo chown -R "$(id -u -n)" ./
 chmod -R +rw ./
-mkdir packages
-mkdir binaries
-mkdir apps
-mkdir bakery
+mkdir .cache apps bakery binaries bin packages
+
+echo "Adding toaster to path..."
+
+echo "Toaster has been installed! 🍞 :)"
