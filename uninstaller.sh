1297bc3f72c56b456937bf57e25b66d73f3ba128
wxllow <35239049+wxllow@users.noreply.github.com>
2022-04-29 21:23:25 -0400

    Better (and safer) uninstall script

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,1 +1,15 @@
-sudo rm -rf /opt/toaster
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+RESET='\033[0m' # No Color
+
+printf "${RED}Are you sure you want to uninstall toaster? (THIS WILL REMOVE ALL YOUR PACKAGES AS WELL) ${RESET}"
+read -r
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Toaster will not be not uninstalled!"
+    exit 1
+fi
+
+echo "Uninstalling toaster... 😔"
+
+rm -r ~/.toaster
