2001b0d6e2049e508733db657778bde5eafded0e
wxllow <willow@wxllow.dev>
2022-05-10 15:51:42 -0400

    Fix colors crossplatform

--- installer.sh
+++ installer.sh
@@ -6,6 +6,12 @@ YELLOW='\033[1;33m'
 MAGENTA='\033[1;35m'
 RESET='\033[0m' # No Color
 
+# Echo function
+# This is because `echo` doesnt work for colors on linux and `echo -e` which fixes it on linux makes mac echo the "-e" part
+secho() {
+    printf "${*}\n"
+}
+
 # OS
 OS=$(uname)
 
@@ -25,26 +31,26 @@ while getopts ":c" option; do
 done
 
 # Check if toaster is already installed
-[ -d "$HOME/.toaster" ] && echo -e "${GREEN}Toaster is already installed! 🍞${RESET}" && exit
+[ -d "$HOME/.toaster" ] && secho "${GREEN}Toaster is already installed! 🍞${RESET}" && exit
 
 # The actual installler starts here
-echo -e "${GREEN}:: Welcome to the toaster installer! 🍞"
-echo -e ":: This script will download and install toaster and add it to your path${RESET}"
-echo -e "${GREY}Unless you otherwise specified, everything will be installed to $HOME/.toaster${RESET}\n"
+secho "${GREEN}:: Welcome to the toaster installer! 🍞"
+secho ":: This script will download and install toaster and add it to your path${RESET}"
+secho "${GREY}Unless you otherwise specified, everything will be installed to $HOME/.toaster${RESET}\n"
 printf "${MAGENTA}Press enter to continue or Ctrl+C to cancel!${RESET}"
 read -p " "
 
-echo -e "${GREY}Making ~/.toaster directory...${RED}"
+secho "${GREY}Making ~/.toaster directory...${RED}"
 mkdir ~/.toaster
 
 # Download
 if $download; then
     SOURCE_DIR="$HOME/.toaster/toaster"
 
-    echo -e "${GREEN}Downloading toaster...${RESET}"
+    secho "${GREEN}Downloading toaster...${RESET}"
     git clone https://github.com/lovebagels/toaster ~/.toaster/toaster || exit 1
 else
-    echo -e "${YELLOW}Using toaster from current directory...${RESET}"
+    secho "${YELLOW}Using toaster from current directory...${RESET}"
 fi
 
 # Download/install dependencies
@@ -60,28 +66,28 @@ python3 -m pip install \
     validators
 
 # Install
-echo -e "${GREY}Copying default bakeries....${RED}"
+secho "${GREY}Copying default bakeries....${RED}"
 cp $SOURCE_DIR/defaults/bakery.json ~/.toaster/bakery.json
 
-echo -e "${GREY}Making subdirectories...${RED}"
+secho "${GREY}Making subdirectories...${RED}"
 cd ~/.toaster
 mkdir .cache apps bakery binaries bin packages package_data
 printf $RESET
 
 if [[ "$PATH" =~ (^|:)"$HOME/.toaster/bin"(:|$) ]]; then
-    echo -e "${YELLOW}Toaster is already in your path. Horray! 🎉${RESET}"
+    secho "${YELLOW}Toaster is already in your path. Horray! 🎉${RESET}"
 else
-    echo -e "Adding toaster to your path..."
+    secho "Adding toaster to your path..."
 
     pathexport='\n# Add toaster to path :)\nexport PATH="$HOME/.toaster/bin:$PATH"\n'
-    echo $pathexport >>~/.zshrc
-    echo $pathexport >>~/.bashrc
+    secho $pathexport >>~/.zshrc
+    secho $pathexport >>~/.bashrc
 
-    echo -e "${GREEN}Toaster was added to your path. Horray! 🎉${RESET}"
+    secho "${GREEN}Toaster was added to your path. Horray! 🎉${RESET}"
 fi
 
 # Link toaster itself to toaster PATH
 ln -sf $SOURCE_DIR/src/toaster/cli.py ~/.toaster/bin/toaster
 chmod +x ~/.toaster/bin/toaster
 
-echo -e "${GREEN}Toaster has been installed! 🍞 :)${RESET}"
+secho "${GREEN}Toaster has been installed! 🍞 :)${RESET}"
