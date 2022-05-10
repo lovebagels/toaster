7099ab5aca2add610b72a0b612730221ef3a835a
wxllow <willow@wxllow.dev>
2022-05-10 13:17:57 -0400

    Use echo -e

--- installer.sh
+++ installer.sh
@@ -25,26 +25,26 @@ while getopts ":c" option; do
 done
 
 # Check if toaster is already installed
-[ -d "$HOME/.toaster" ] && echo "${GREEN}Toaster is already installed! 🍞${RESET}" && exit
+[ -d "$HOME/.toaster" ] && echo -e "${GREEN}Toaster is already installed! 🍞${RESET}" && exit
 
 # The actual installler starts here
-echo "${GREEN}:: Welcome to the toaster installer! 🍞"
-echo ":: This script will download and install toaster and add it to your path${RESET}"
-echo "${GREY}Unless you otherwise specified, everything will be installed to $HOME/.toaster${RESET}\n"
+echo -e "${GREEN}:: Welcome to the toaster installer! 🍞"
+echo -e ":: This script will download and install toaster and add it to your path${RESET}"
+echo -e "${GREY}Unless you otherwise specified, everything will be installed to $HOME/.toaster${RESET}\n"
 printf "${MAGENTA}Press enter to continue or Ctrl+C to cancel!${RESET}"
 read -p " "
 
-echo "${GREY}Making ~/.toaster directory...${RED}"
+echo -e "${GREY}Making ~/.toaster directory...${RED}"
 mkdir ~/.toaster
 
 # Download
 if $download; then
     SOURCE_DIR="$HOME/.toaster/toaster"
 
-    echo "${GREEN}Downloading toaster...${RESET}"
+    echo -e "${GREEN}Downloading toaster...${RESET}"
     git clone https://github.com/lovebagels/toaster ~/.toaster/toaster || exit 1
 else
-    echo "${YELLOW}Using toaster from current directory...${RESET}"
+    echo -e "${YELLOW}Using toaster from current directory...${RESET}"
 fi
 
 # Download/install dependencies
@@ -60,28 +60,28 @@ python3 -m pip install \
     validators
 
 # Install
-echo "${GREY}Copying default bakeries....${RED}"
+echo -e "${GREY}Copying default bakeries....${RED}"
 cp $SOURCE_DIR/defaults/bakery.json ~/.toaster/bakery.json
 
-echo "${GREY}Making subdirectories...${RED}"
+echo -e "${GREY}Making subdirectories...${RED}"
 cd ~/.toaster
 mkdir .cache apps bakery binaries bin packages package_data
 printf $RESET
 
 if [[ "$PATH" =~ (^|:)"$HOME/.toaster/bin"(:|$) ]]; then
-    echo "${YELLOW}Toaster is already in your path. Horray! 🎉${RESET}"
+    echo -e "${YELLOW}Toaster is already in your path. Horray! 🎉${RESET}"
 else
-    echo "Adding toaster to your path..."
+    echo -e "Adding toaster to your path..."
 
     pathexport='\n# Add toaster to path :)\nexport PATH="$HOME/.toaster/bin:$PATH"\n'
     echo $pathexport >>~/.zshrc
     echo $pathexport >>~/.bashrc
 
-    echo "${GREEN}Toaster was added to your path. Horray! 🎉${RESET}"
+    echo -e "${GREEN}Toaster was added to your path. Horray! 🎉${RESET}"
 fi
 
 # Link toaster itself to toaster PATH
 ln -sf $SOURCE_DIR/src/toaster/cli.py ~/.toaster/bin/toaster
 chmod +x ~/.toaster/bin/toaster
 
-echo "${GREEN}Toaster has been installed! 🍞 :)${RESET}"
+echo -e "${GREEN}Toaster has been installed! 🍞 :)${RESET}"
