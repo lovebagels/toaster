4cce9f2bfe2c315043be2a4a610093588c8d375f
wxllow <willow@wxllow.dev>
2022-05-17 12:40:21 -0400

    amazing!

--- uninstaller.sh
+++ uninstaller.sh
@@ -2,7 +2,7 @@ RED='\033[1;31m'
 GREEN='\033[1;32m'
 RESET='\033[0m' # No Color
 
-printf "${RED}Are you sure you want to uninstall toaster? (THIS WILL REMOVE ALL YOUR PACKAGES AS WELL) ${RESET}"
+printf "${RED}Are you sure you want to uninstall toaster? (THIS WILL REMOVE ALL YOUR AMAZING PACKAGES AS WELL) ${RESET}"
 read -r
 
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
